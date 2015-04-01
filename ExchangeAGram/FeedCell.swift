bbbefb0712716703302b7a3c751f50497300ed87
Brett Wallace <bwallace90@gmail.com>
2015-03-31 21:27:32 -0400

    Set up FeedCell

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Brett Wallace on 3/31/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
