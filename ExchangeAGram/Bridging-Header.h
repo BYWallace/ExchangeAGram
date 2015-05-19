2852111e4aa40eeb0867a8139589323718f4f077
Brett Wallace <bwallace90@gmail.com>
2015-05-19 17:11:16 -0400

    Install Facebook SDK and prep project

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Brett Wallace on 5/19/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
