dbae7130815edf2eb2d915dff692f87509133c4c
Brett Wallace <bwallace90@gmail.com>
2015-05-24 14:00:35 -0400

    Set up MapKit framework and MapViewController

---
+++ ExchangeAGram/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  ExchangeAGram
+//
+//  Created by Brett Wallace on 5/24/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
