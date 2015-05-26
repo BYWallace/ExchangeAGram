c3dad87f5e624074a4c5a2dda44d6cb0ef204e9d
Brett Wallace <bwallace90@gmail.com>
2015-05-26 17:20:43 -0400

    Update FeedItem model to record latitude and longitude

--- FeedItem.swift
+++ FeedItem.swift
@@ -2,18 +2,20 @@
 //  FeedItem.swift
 //  ExchangeAGram
 //
-//  Created by Brett Wallace on 4/27/15.
+//  Created by Brett Wallace on 5/26/15.
 //  Copyright (c) 2015 Brett Wallace. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-@objc (FeedItem)
+@objc(FeedItem)
 class FeedItem: NSManagedObject {
 
     @NSManaged var caption: String
     @NSManaged var image: NSData
     @NSManaged var thumbNail: NSData
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
 
 }
