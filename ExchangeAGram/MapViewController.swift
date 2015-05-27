a723c97286d6226f30d119c6814a3da0e8df76b2
Brett Wallace <bwallace90@gmail.com>
2015-05-27 14:12:57 -0400

    Set up NSFetchRequest for feed items in MapViewController

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
@@ -17,17 +18,24 @@ class MapViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegionMake(location, span)
-        mapView.setRegion(region, animated: true)
+        let request = NSFetchRequest(entityName: "FeedItem")
+        let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext!
+        var error:NSError?
+        let itemArray = context.executeFetchRequest(request, error: &error)
+        println(error)
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Canal Saint-Martin"
-        annotation.subtitle = "Paris"
-        
-        mapView.addAnnotation(annotation)
+//        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        let region = MKCoordinateRegionMake(location, span)
+//        mapView.setRegion(region, animated: true)
+//        
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = location
+//        annotation.title = "Canal Saint-Martin"
+//        annotation.subtitle = "Paris"
+//        
+//        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
